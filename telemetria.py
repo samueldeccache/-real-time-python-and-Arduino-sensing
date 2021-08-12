9b2d959c344bba7c1d97cef5e5d2b3996fb4037d
Samuel Deccache <65060007+samueldeccache@users.noreply.github.com>
2021-08-12 04:26:03 -0300

    Update telemetria.py

--- telemetria.py
+++ telemetria.py
@@ -42,16 +42,6 @@ def hl(numbers):
         x = float(x)
         y.append(x)
     return y
-'''def leitura_serial(leitura_serial):
-    vet=[]
-    for i in range(0,10):
-        leitura_serial = conexao.readline()
-        leitura_limpa=leitura_serial.split()
-        leitura_limpa1=str(leitura_limpa).strip('[]')
-        leitura_limpa2=leitura_limpa1.replace('b','')
-        leitura_limpa3=str(leitura_limpa2).strip("''")
-        vet.append(leitura_limpa3)
-    return vet '''
 vet=[]
     
 while True:
@@ -85,34 +75,6 @@ while True:
     vet2=splited[1]
     x1=hl(vet1)
     x2=hl(vet2)
-    '''x=hl(vet)
-    #dividindo os vetores. A ideia é que cada valor lido 
-    #do arduino representa um sensoriamento de cada linha
-    # exemplo: o vetor1 guarda os valores de luminosidade e o vetor2 de distancia
-    n=2
-    splited = [vet[i::n] for i in range(n)]
-    y=[]
-    vet1=splited[0]
-    vet2=splited[1]
-    x1=hl(vet1)
-    x2=hl(vet2)
-    #t= input('deseja continuar?')'''
-    '''if t == 'sim':
-        for i in range(0,len(x2)):
-            y.append(i)
-        plt.plot(y,x1)
-        plt.plot(y,x2)
-        plt.plot(y,x3)
-        plt.title('Distancia x tempo')
-        plt.show()
-    else:
-        for i in range(0,len(x2)):
-            y.append(i)
-        plt.plot(y, x1)
-        plt.plot(y,x2)
-        plt.plot(y,x3)
-        plt.title('Distancia x tempo')
-        plt.show()'''
     for i in range(0,len(x2)):
             y.append(i)
     cont+=1
